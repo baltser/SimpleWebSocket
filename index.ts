4c1525a861f28d453a32d38548c5e0b00c081f87
balzer <antonbaltser@yahoo.com>
2023-06-30 14:20:02 +0000

    simple webSocket chat

--- index.ts
+++ index.ts
@@ -2,27 +2,30 @@ import express, {Express, Request, Response} from 'express';
 import * as http from 'http';
 import * as WebSocket from 'ws';
 import * as path from "path";
-
+//HTTP Server
 const app:Express = express();
 
 const server = http.createServer(app);
-
-const ws = new WebSocket.Server({ server });
-//HTTP Server
 app.get('/', (req: Request, res: Response) => res.sendFile(
     path.join(__dirname, 'public', 'simple-websocket-client.html')
-))
+));
 
-ws.on('connection', (ws: WebSocket) => {
-    ws.on('message', (message: string) => {
-        console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+const ws = new WebSocket.Server({ server });
+ws.on('connection', (connection: WebSocket, req) => {
+    const ip = req.socket.remoteAddress;
+    console.log(`Connected ${ip}`);
+    connection.on('message', (message) => {
+        console.dir('Received: ' + message);
+        for (const client of ws.clients) {
+            if (client.readyState !== WebSocket.OPEN) continue;
+            if (client === connection) continue;
+            client.send(message, { binary: false });
+        }
     });
-
-    ws.send('Hi there, I am a WebSocket server');
+    connection.on('close', () => {
+        console.log(`Disconnected ${ip}`);
+    });
 });
-
-//start our server
 server.listen(8000, 'localhost',() => {
     console.log(`Server started on port localhost:8000`);
 });
